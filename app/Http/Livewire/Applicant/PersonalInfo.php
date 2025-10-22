b411172396b9c1033da60db68095f54c8af7cc68
Tristan Sarceda <orbinobrian0506@gmail.com>
2025-10-22 15:18:38 +0800

    uupdare

--- app/Http/Livewire/Applicant/PersonalInfo.php
+++ app/Http/Livewire/Applicant/PersonalInfo.php
@@ -6,32 +6,33 @@ use Livewire\Component;
 use App\Models\PersonalInformation;
 use WireUi\Traits\Actions;
 use Livewire\WithFileUploads;
+
 class PersonalInfo extends Component
 {
     use Actions, WithFileUploads;
-    public $type_id,$first_name, $middle_name,$last_name,$extension,$present_address,$permanent_address,$phone_number,$date_of_birth,$place_of_birth,$age,$tribe,$religion,$nationality,$citizenship,$photo,$sex;
+    public $type_id, $first_name, $middle_name, $last_name, $extension, $present_address, $permanent_address, $phone_number, $date_of_birth, $place_of_birth, $age, $tribe, $religion, $nationality, $citizenship, $photo, $sex;
     public $personal_information;
     protected $rules = [
-        'type_id'=>'required|in:1,2',
-        'first_name'=>'required',
-        'middle_name'=>'nullable',
-        'last_name'=>'required',
-        'extension'=>'nullable',
-        'present_address'=>'required',
-        'permanent_address'=>'required',
-        'phone_number'=>'required',
-        'date_of_birth'=>'required',
-        'place_of_birth'=>'required',
-        'age'=>'required|gte:16',
-        'tribe'=>'required',
-        'religion'=>'required',
-        'nationality'=>'required',
-        'citizenship'=>'nullable',
-        'photo'=>'required|image|mimes:png,jpg|max:100000',
-        'sex'=>'required',
+        'type_id' => 'required|in:1,2',
+        'first_name' => 'required',
+        'middle_name' => 'nullable',
+        'last_name' => 'required',
+        'extension' => 'nullable',
+        'present_address' => 'required',
+        'permanent_address' => 'required',
+        'phone_number' => 'required',
+        'date_of_birth' => 'required',
+        'place_of_birth' => 'required',
+        'age' => 'required|gte:16',
+        'tribe' => 'required',
+        'religion' => 'required',
+        'nationality' => 'required',
+        'citizenship' => 'nullable',
+        'photo' => 'required|image|mimes:png,jpg|max:100000',
+        'sex' => 'required',
     ];
-    protected $validationAttributes=['type_id'=>'type'];
-    protected $listeners = ['done-all'=>'$refresh'];
+    protected $validationAttributes = ['type_id' => 'type'];
+    protected $listeners = ['done-all' => '$refresh'];
     public function mount()
     {
         $this->loadPersonalInformation();
@@ -48,102 +49,132 @@ class PersonalInfo extends Component
         $this->age = \Carbon\Carbon::now()->diffInYears($birthday);
     }
 
-    public function create()
-    {
-        $this->validate();
-        PersonalInformation::create([
-            'user_id'=>auth()->user()->id,
-            'type_id'=>$this->type_id,
-            'first_name'=>$this->first_name,
-            'middle_name'=>$this->middle_name,
-            'last_name'=>$this->last_name,
-            'extension'=>$this->extension,
-            'present_address'=>$this->present_address,
-            'permanent_address'=>$this->permanent_address,
-            'phone_number'=>$this->phone_number,
-            'date_of_birth'=>$this->date_of_birth,
-            'place_of_birth'=>$this->place_of_birth,
-            'age'=>$this->age,
-            'tribe'=>$this->tribe,
-            'religion'=>$this->religion,
-            'nationality'=>$this->nationality,
-            'citizenship' => $this->nationality,
-            'photo' => $this->photo->store('photos','public'),
-            'sex' => $this->sex,
-        ]);
-        auth()->user()->update([
-            'type_id'=>$this->type_id,
-        ]);
-        $this->dispatchBrowserEvent('done-pi');
-        $this->emit('piUpdated');
-        $this->notification([
-            'title'=>'Success',
-            'description'=>'Personal Information has beed added',
-            'icon'=>'success',
-        ]);
+   public function create()
+{
 
-        $this->loadPersonalInformation();
+    \Log::info('Photo upload debug', [
+    'photo' => $this->photo,
+    'has_error' => $this->photo?->getError(),
+]);
+
+    $validated = $this->validate([
+        'type_id' => 'required|in:1,2',
+        'first_name' => 'required',
+        'middle_name' => 'nullable',
+        'last_name' => 'required',
+        'extension' => 'nullable',
+        'present_address' => 'required',
+        'permanent_address' => 'required',
+        'phone_number' => 'required',
+        'date_of_birth' => 'required|date',
+        'place_of_birth' => 'required',
+        'age' => 'required|gte:16',
+        'tribe' => 'required',
+        'religion' => 'required',
+        'nationality' => 'required',
+        'citizenship' => 'nullable',
+        'photo' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        'sex' => 'required',
+    ]);
+
+    $photoPath = null;
+
+    if ($this->photo && !$this->photo->getError()) {
+        try {
+            $photoPath = $this->photo->store('photos', 'public');
+        } catch (\Exception $e) {
+            $this->notification([
+                'title' => 'Upload Error',
+                'description' => 'Failed to upload photo: '.$e->getMessage(),
+                'icon' => 'error',
+            ]);
+            return;
+        }
     }
+
+    PersonalInformation::create(array_merge($validated, [
+        'user_id' => auth()->id(),
+        'photo' => $photoPath,
+        'citizenship' => $this->nationality,
+    ]));
+
+    auth()->user()->update(['type_id' => $this->type_id]);
+
+    $this->dispatchBrowserEvent('done-pi');
+    $this->emit('piUpdated');
+    $this->notification([
+        'title' => 'Success',
+        'description' => 'Personal Information has been added successfully.',
+        'icon' => 'success',
+    ]);
+
+    $this->loadPersonalInformation();
+}
+
+
     public function update()
     {
-        $personal_information = PersonalInformation::where('user_id',auth()->user()->id)->first();
+        $personal_information = PersonalInformation::where('user_id', auth()->user()->id)->first();
         $this->validate([
-            'type_id'=>'required|in:1,2',
-            'first_name'=>'required',
-            'middle_name'=>'nullable',
-            'last_name'=>'required',
-            'extension'=>'nullable',
-            'present_address'=>'required',
-            'permanent_address'=>'required',
-            'phone_number'=>'required',
-            'date_of_birth'=>'required',
-            'place_of_birth'=>'required',
-            'age'=>'required|gte:16',
-            'tribe'=>'required',
-            'religion'=>'required',
-            'nationality'=>'required',
-            'citizenship'=>'nullable',
-            'photo'=> 'nullable',
-            'sex'=>'required',
+            'type_id' => 'required|in:1,2',
+            'first_name' => 'required',
+            'middle_name' => 'nullable',
+            'last_name' => 'required',
+            'extension' => 'nullable',
+            'present_address' => 'required',
+            'permanent_address' => 'required',
+            'phone_number' => 'required',
+            'date_of_birth' => 'required',
+            'place_of_birth' => 'required',
+            'age' => 'required|gte:16',
+            'tribe' => 'required',
+            'religion' => 'required',
+            'nationality' => 'required',
+            'citizenship' => 'nullable',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'sex' => 'required',
         ]);
+        $photoPath = $personal_information->photo;
+        if ($this->photo && !$this->photo->getError()) {
+            $photoPath = $this->photo->store('photos', 'public');
+        }
         $personal_information->update([
-            'type_id'=>$this->type_id,
-            'first_name'=>$this->first_name,
-            'middle_name'=>$this->middle_name,
-            'last_name'=>$this->last_name,
-            'extension'=>$this->extension,
-            'present_address'=>$this->present_address,
-            'permanent_address'=>$this->permanent_address,
-            'phone_number'=>$this->phone_number,
-            'date_of_birth'=>$this->date_of_birth,
-            'place_of_birth'=>$this->place_of_birth,
-            'age'=>$this->age,
-            'tribe'=>$this->tribe,
-            'religion'=>$this->religion,
-            'nationality'=>$this->nationality,
+            'type_id' => $this->type_id,
+            'first_name' => $this->first_name,
+            'middle_name' => $this->middle_name,
+            'last_name' => $this->last_name,
+            'extension' => $this->extension,
+            'present_address' => $this->present_address,
+            'permanent_address' => $this->permanent_address,
+            'phone_number' => $this->phone_number,
+            'date_of_birth' => $this->date_of_birth,
+            'place_of_birth' => $this->place_of_birth,
+            'age' => $this->age,
+            'tribe' => $this->tribe,
+            'religion' => $this->religion,
+            'nationality' => $this->nationality,
             'citizenship' => $this->nationality,
-            'photo' => $this->photo ? $this->photo->store('photos','public') : $personal_information->photo,
+            'photo' => $photoPath,
             'sex' => $this->sex,
         ]);
         auth()->user()->update([
-            'type_id'=>$this->type_id,
+            'type_id' => $this->type_id,
         ]);
         $this->dispatchBrowserEvent('done-pi');
         $this->emit('piUpdated');
         $this->notification([
-            'title'=>'Success',
-            'description'=>'Personal Information has beed updated',
-            'icon'=>'success',
+            'title' => 'Success',
+            'description' => 'Personal Information has beed updated',
+            'icon' => 'success',
         ]);
         $this->loadPersonalInformation();
-
     }
     public function loadPersonalInformation()
     {
         $this->first_name = auth()->user()->first_name;
         $this->middle_name = auth()->user()->middle_name;
         $this->last_name = auth()->user()->last_name;
-        $this->personal_information= PersonalInformation::where('user_id',auth()->user()->id)->first();
+        $this->personal_information = PersonalInformation::where('user_id', auth()->user()->id)->first();
         if ($this->personal_information) {
             $this->type_id = $this->personal_information->type_id;
             $this->first_name = $this->personal_information->first_name;
